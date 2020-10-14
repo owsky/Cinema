aa4fd0117bf0fa764e369b222188c239ba2d9b06
Nicolò Bertocco <nick.bertocco@gmail.com>
2020-10-14 21:59:50 +0200

    Fixed query serial

--- db/sql/init.sql
+++ db/sql/init.sql
@@ -1,50 +1,50 @@
 -- ROOMS
-INSERT INTO public.rooms(rooms_id, rooms_name) VALUES
-    (1, 'Room A'),
-    (2, 'Room B');
+INSERT INTO public.rooms(rooms_name) VALUES
+    ('Room A'),
+    ('Room B');
 
 -- SEATS
 
-INSERT INTO public.seats(seats_id, seats_name, seats_room) VALUES
-    (1, '1A', 1),
-    (2, '2A', 1),
-    (3, '3A', 1),
-    (4, '4A', 1),
-    (5, '5A', 1),
-    (6, '6A', 1),
-    (7, '7A', 1),
-    (8, '8A', 1),
-    (9, '9A', 1),
-    (10, '10A', 1),
-    (11, '1B', 2),
-    (12, '2B', 2),
-    (13, '3B', 2),
-    (14, '4B', 2),
-    (15, '5B', 2),
-    (16, '6B', 2),
-    (17, '7B', 2),
-    (18, '8B', 2),
-    (19, '9B', 2),
-    (20, '10B', 2);
+INSERT INTO public.seats(seats_name, seats_room) VALUES
+    ('1A', 1),
+    ('2A', 1),
+    ('3A', 1),
+    ('4A', 1),
+    ('5A', 1),
+    ('6A', 1),
+    ('7A', 1),
+    ('8A', 1),
+    ('9A', 1),
+    ('10A', 1),
+    ('1B', 2),
+    ('2B', 2),
+    ('3B', 2),
+    ('4B', 2),
+    ('5B', 2),
+    ('6B', 2),
+    ('7B', 2),
+    ('8B', 2),
+    ('9B', 2),
+    ('10B', 2);
 
 -- DIRECTORS
-INSERT INTO public.directors(directors_id, directors_name) VALUES
-    (1, 'Martin Scorsese'),
-    (2, 'Christopher Nolan'),
-    (3, 'Stanley Kubrick');
+INSERT INTO public.directors(directors_name) VALUES
+    ('Martin Scorsese'),
+    ('Christopher Nolan'),
+    ('Stanley Kubrick');
 
 -- MOVIES
-INSERT INTO public.movies(movies_id, movies_title, movies_duration, movies_genre, movies_synopsis, movies_director) VALUES
-    (1, 'Shutter Island', 139, 'Thriller', 'In 1954, a U.S. Marshal investigates the disappearance of a murderer who escaped from a hospital for the criminally insane.', 1),
-    (2, 'Inception', 162, 'Action', 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', 2),
-    (3, '2001: A Space Odyssey', 164, 'Sci-Fi', 'After discovering a mysterious artifact buried beneath the Lunar surface, mankind sets off on a quest to find its origins with help from intelligent supercomputer H.A.L. 9000.', 3),
-    (4, 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb', 102, 'Comedy', 'An insane general triggers a path to nuclear holocaust that a War Room full of politicians and generals frantically tries to stop.', 3);
+INSERT INTO public.movies(movies_title, movies_duration, movies_genre, movies_synopsis, movies_director) VALUES
+    ('Shutter Island', 139, 'Thriller', 'In 1954, a U.S. Marshal investigates the disappearance of a murderer who escaped from a hospital for the criminally insane.', 1),
+    ('Inception', 162, 'Action', 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', 2),
+    ('2001: A Space Odyssey', 164, 'Sci-Fi', 'After discovering a mysterious artifact buried beneath the Lunar surface, mankind sets off on a quest to find its origins with help from intelligent supercomputer H.A.L. 9000.', 3),
+    ('Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb', 102, 'Comedy', 'An insane general triggers a path to nuclear holocaust that a War Room full of politicians and generals frantically tries to stop.', 3);
 
 -- ACTORS
-INSERT INTO public.actors(actors_id, actors_fullname) VALUES
-    (1, 'Leonardo DiCaprio'),
-    (2, 'Keir Dullea'),
-    (3, 'Peter Sellers');
+INSERT INTO public.actors(actors_fullname) VALUES
+    ('Leonardo DiCaprio'),
+    ('Keir Dullea'),
+    ('Peter Sellers');
 
 -- CAST
 INSERT INTO public.cast(cast_movie, cast_actor) VALUES
@@ -54,24 +54,24 @@ INSERT INTO public.cast(cast_movie, cast_actor) VALUES
     (4, 3);
 
 -- USERS
-INSERT INTO public.users(users_id, users_email, users_name, users_surname, users_pwd, users_is_manager) VALUES
-    (1, 'jack@aol.com', 'Jack', 'Boeing', 'passwordsicura', False),
-    (2, 'jane@hotmail.com', 'Jane', 'Accounting', 'bookkeepingaf', False),
-    (3, 'anonymous@gmail.com', 'Anon', 'Chan', 'imgoodlulz', False),
-    (4, 'megamanager@gmail.com', 'Kim', 'Schmitz', 'fuckfbi', True),
-    (5, 'manager', 'manager', 'manager', 'manager', True),
-    (6, 'user', 'user', 'user', 'user', False);
+INSERT INTO public.users(users_email, users_name, users_surname, users_pwd, users_is_manager) VALUES
+    ('jack@aol.com', 'Jack', 'Boeing', 'passwordsicura', False),
+    ('jane@hotmail.com', 'Jane', 'Accounting', 'bookkeepingaf', False),
+    ('anonymous@gmail.com', 'Anon', 'Chan', 'scriptkiddie', False),
+    ('megamanager@gmail.com', 'Kim', 'Schmitz', 'megamanager', True),
+    ('manager', 'manager', 'manager', 'manager', True),
+    ('user', 'user', 'user', 'user', False);
 
 -- PROJECTIONS
-INSERT INTO public.projections(projections_id, projections_movie, projections_date_time, projections_room) VALUES
-    (1, 1, '2020-08-01 21:00:00', 1),
-    (2, 2, '2020-08-01 21:00:00', 2),
-    (3, 3, '2020-08-02 21:00:00', 1),
-    (4, 4, '2020-08-02 21:00:00', 2);
+INSERT INTO public.projections(projections_movie, projections_date_time, projections_room) VALUES
+    (1, '2020-08-01 21:00:00', 1),
+    (2, '2020-08-01 21:00:00', 2),
+    (3, '2020-08-02 21:00:00', 1),
+    (4, '2020-08-02 21:00:00', 2);
 
 -- TICKETS
-INSERT INTO public.tickets(tickets_id, tickets_user, tickets_projection, tickets_seat) VALUES
-    (1, 1, 1, 1),
-    (2, 1, 2, 1),
-    (3, 2, 1, 2),
-    (4, 3, 4, 1);
+INSERT INTO public.tickets(tickets_user, tickets_projection, tickets_seat) VALUES
+    (1, 1, 1),
+    (1, 2, 1),
+    (2, 1, 2),
+    (3, 4, 1);
