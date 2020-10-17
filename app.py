51aaa5bd547ccd04c3d8f198c2f404424455003a
giuliahuang <ruoxingiuliahuang@gmail.com>
2020-10-17 23:19:21 +0200

    add_movie, update_movie not completed, changes to index.html

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, abort, flash
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user, AnonymousUserMixin
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user, \
+    AnonymousUserMixin
 from sqlalchemy import select, text
 import secrets
 from schema import engine, actors, cast, directors, movies, rooms, seats, tickets, users, projections
@@ -50,7 +51,8 @@ def home():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        if not request.form['name'] or not request.form['surname'] or not request.form['email'] or not request.form['pwd']:
+        if not request.form['name'] or not request.form['surname'] or not request.form['email'] or not request.form[
+            'pwd']:
             flash("Missing information")
         if user_by_email(request.form['email']) is not None:
             flash("There's already an account set up to use this email address")
@@ -121,26 +123,71 @@ def movie_info(title):
 def movie_manager():
     if not current_user.is_manager:
         abort(403)
-    return render_template("manager/manager.html")
+    else:
+        conn = engine.connect()
+        s = text("""SELECT * FROM public.projections JOIN public.movies ON (projections_movie=movies_id)""")
+        rs = conn.execute(s)
+        u = rs.fetchall()
+        conn.close()
+        for p in u:
+            date = p.projections_date_time.strftime("%m/%d/%Y")
+            hour = p.projections_date_time.strftime("%H:%M:%S")[:5]
+            return render_template("manager/manager.html", projection=u, date=date, hour=hour)
 
 
-@app.route('/movie_manager/add_movie', methods=['GET', 'POST'])
+@app.route('/manager/')
+@login_required
+def projection():
+    conn = engine.connect()
+    s = text("""
+    SELECT * 
+    FROM public.projections
+    where projections_movie=5""")
+    rs = conn.execute(s)
+    u = rs.fetchall()
+    conn.close()
+    return render_template('manager/manager.html', proj=u)
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+@login_required
+def update_movie(id):
+    if not current_user.is_manager:
+        abort(403)
+    conn = engine.connect()
+    movie_to_update = movies.get(id)
+    if request.method == "POST":
+        movie_to_update.title = request.form['title']
+        movie_to_update.genre = request.form['genre']
+        movie_to_update.duration = request.form['duration']
+        movie_to_update.synopsis = request.form['synopsis']
+        movie_to_update.director = request.form['director']
+        u = movies.update().values(movies_title=movie_to_update.title, movies_genre=movie_to_update.genre,
+                                   movies_duration=movie_to_update.duration,
+                                   movies_synopsis=movie_to_update.synopsis, movies_director=movie_to_update.director). \
+            where(movies.c.movies_id == id)
+        conn.execute(u)
+        conn.close()
+        return redirect(url_for('home'))
+    return render_template('update_movie.html', movie_to_update=movie_to_update)
+
+
+@app.route('/add_movie', methods=['GET', 'POST'])
 @login_required
 def add_movie():
     if not current_user.is_manager:
         abort(403)
-
     if request.method == 'POST':
         conn = engine.connect()
         ins = movies.insert()
         conn.execute(ins, [
             {"movies_title": request.form['title'], "movies_genre": request.form['genre'],
+             "movies_duration": request.form['duration'],
              "movies_synopsis": request.form['synopsis'], "movies_director": request.form['director']}
         ])
         conn.close()
-        return render_template("manager/movie_manager.html", movies=get_projections(None))
-    else:
-        return render_template("manager/add_movie.html")
+        return render_template("manager/movie_manager.html")
+    return render_template("manager/add_movie.html")
 
 
 @app.route('/session_manager')
@@ -245,7 +292,7 @@ def reserve_ticket(proj_id):
         sold = rs1.fetchone()
 
         s2 = text("INSERT INTO public.tickets(tickets_user, tickets_projection, tickets_seat) VALUES (:e1, :e2, :e3)")
-        conn.execute(s2, e1=current_user.id, e2=proj_id, e3=sold[0]+1)
+        conn.execute(s2, e1=current_user.id, e2=proj_id, e3=sold[0] + 1)
         return True
 
 
